97788a71754d374321fad10d849e69c5210b9cef
LDG <wotn2517@naver.com>
2021-08-15 13:23:28 +0900

    기본 구현

--- projectbyreact/src/App.jsx
+++ projectbyreact/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Login from './views/home/Login';
-import Main from './views/home/Main';
-import Nav from './views/public/Nav';
+import Login from './components/views/home/Login';
+import Main from './components/views/home/Main';
+import Nav from './components/views/public/Nav';
+import ReduxApp01 from './practice/ReduxApp01';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+      <ReduxApp01/>
         <Nav/>
           <Switch>
             <Route exact path = "/Login" component = {Login}/>
